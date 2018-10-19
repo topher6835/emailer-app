55863d81aaa23ad8af5d07866caff7e730bc6a58
Chris <sears.topher@gmail.com>
2018-10-19 12:10:17 -0400

    update google strategy setting

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,6 @@
 // keys.js
-
 if(process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
 } else {
   module.exports = require('./dev');
 }
-
-//  prod
-//  1047435571736-vpbs5lt4h96eco98ba0dvodffsbi6hjs.apps.googleusercontent.com
-//  FbtLz4gva6o6-4JP1NpgWJtA
